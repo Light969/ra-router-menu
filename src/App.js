8fec7dbe3a430829c24ee6217ed29fddb3ee2f7e
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-10-08 16:55:43 +0300

    ra-router-menu-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import HomePage from './react-component/HomePage';
+import DriftPage from './react-component/DriftPage';
+import ForzaPage from './react-component/ForzaPage';
+import TimeAttackPage from './react-component/TimeAttackPage';
 
-function App() {
+function Menu() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav className="menu">
+      <NavLink className="menu__item" activeClassName="menu__item-active" exact to="/">Главная</NavLink>
+      <NavLink className="menu__item" activeClassName="menu__item-active" to="/drift">Дрифт-такси</NavLink>
+      <NavLink className="menu__item" activeClassName="menu__item-active" to="/timeattack">Time Attack</NavLink>
+      <NavLink className="menu__item" activeClassName="menu__item-active" to="/forza">Forza Karting</NavLink>
+    </nav>
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/drift" component={DriftPage} />
+          <Route path="/timeattack" component={TimeAttackPage} />
+          <Route path="/forza" component={ForzaPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
